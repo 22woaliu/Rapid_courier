7aec9cb6b54bd36113af65b8d924dd3a3f734a62
Woaliu Hasan <hasan2207090@stud.kuet.ac.bd>
2025-12-09 00:43:22 +0600

    second

---
+++ src/main/java/com/example/rapid_courier/Launcher.java
@@ -0,0 +1,9 @@
+package com.example.rapid_courier;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(HelloApplication.class, args);
+    }
+}
